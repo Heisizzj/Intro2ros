57621458c26ddc2fe082513594e566b974caca9a
ge87puz <shaolong.tang@tum.de>
2024-07-25 22:26:14 +0200

    height check

--- AutonomousQuadruped/README.md
+++ AutonomousQuadruped/README.md
@@ -1,30 +1,5 @@
-## Getting Started
+#更新了两个检测点是否被占用的测试
+1.二维网格是否被占用测试（输出bool值）
+2.检测高度 超过阈值则报告
 
-
-1. Copy the src-folder to your repository and build it
-2. Download the Unity Environment: https://syncandshare.lrz.de/getlink/fiLvgiTXetubiN1i4PRjuR/
-3. Unzip the Unity file and copy the files to .../devel/lib/simulation/
-4. Run a test:
-  a.) roslaunch simulation simulation.launch
-  b.) rosrun controller_pkg controller_node
-  
-You will see the quadruped in front of you and it is walking forward. The goal is to navigate the quadruped through the parcour. If you take a look to the source-code of the controller_node you will see how to control the quadruped with the inputs Phase, SkewPhase, Amplitude change, Amplitude back and Frequency (of the legs).
-
-Part of the task is to play with these control inputs in order to make the robot walk. You will have to experiment with the differend commands to pass the parcour. To give you another hint. A possible combination for rotation in spot is: 
-
-Phase = 0
-Skew Phase = 45
-Amplitude Change = 0
-Amplitude Back = 0
-Frequency = 7
-
-
-# Tips
-
-Here are a couple of hints regarding the implementation. The hints are just suggestions; you are free so solve the task differently:
-- Generating point cloud from depth image: use depth_image_proc in http://wiki.ros.org/depth_
-image_proc.
-- Generating occupancy Grid: use Octomap in http://wiki.ros.org/octomap.
-- Please ping us in case you have any questions or if you get stuck in some subtasks.
-- Use a global map as your voxel grid representation. Use a smart resolution for your voxel grid representation (e.g. 1m).
-
+但并不是完全好用 仍在测试中
---
+++ AutonomousQuadruped/src/path_height_checker/src/path_height_checker.cpp
@@ -0,0 +1,325 @@
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <octomap_msgs/Octomap.h>
+#include <octomap/OcTree.h>
+#include <octomap/AbstractOcTree.h>
+#include <octomap_msgs/conversions.h>
+
+#include <nav_msgs/Path.h>
+#include <std_msgs/Bool.h>
+#include <octomap/OccupancyOcTreeBase.h>
+
+/*
+ 
+
+
+class PathOccupancyChecker
+{
+public:
+    PathOccupancyChecker()
+    {
+        // 订阅路径点话题
+        path_sub_ = nh_.subscribe("move_base_Quadruped/NavfnROS/plan", 1, &PathOccupancyChecker::pathCallback, this);
+
+        // 订阅OctoMap话题
+        octomap_sub_ = nh_.subscribe("octomap_full", 1, &PathOccupancyChecker::octomapCallback, this);
+
+        // 发布占用状态
+        occupancy_pub_ = nh_.advertise<std_msgs::Bool>("path_occupancy_status", 1);
+
+        // 初始化标志
+        octree_ = nullptr;
+    }
+
+private:
+    void octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg)
+    {
+        // 将OctoMap消息转换为Octree对象
+        octomap::AbstractOcTree* abstract_tree = octomap_msgs::msgToMap(*msg);
+        octree_ = dynamic_cast<octomap::OcTree*>(abstract_tree);
+    }
+
+    void pathCallback(const nav_msgs::Path::ConstPtr& path)
+    {
+        if (!octree_)
+        {
+            ROS_WARN("OctoMap has not been received yet.");
+            return;
+        }
+
+        bool is_occupied = false;
+
+        // 使用ROS_INFO打印路径点的占用状态
+        for (const auto& pose : path->poses)
+        {
+            octomap::point3d coord(pose.pose.position.x, pose.pose.position.y, 0.0);
+
+            // 查找对应的栅格
+            octomap::OcTreeNode* node = octree_->search(coord);
+
+            if (node)
+            {
+                double occupancy_prob = node->getOccupancy();
+
+                // 判断是否被占用
+                if (occupancy_prob > 0.5) // 占用概率大于0.5表示被占用
+                {
+                    ROS_INFO("Point (%.2f, %.2f) is occupied.", pose.pose.position.x, pose.pose.position.y);
+                    is_occupied = true;
+                }
+                else
+                {
+                    ROS_INFO("Point (%.2f, %.2f) is not occupied.", pose.pose.position.x, pose.pose.position.y);
+                }
+            }
+            else
+            {
+                ROS_INFO("Point (%.2f, %.2f) is not in the map.", pose.pose.position.x, pose.pose.position.y);
+            }
+        }
+
+        // 发布占用状态
+        std_msgs::Bool occupancy_msg;
+        occupancy_msg.data = is_occupied;
+        occupancy_pub_.publish(occupancy_msg);
+    }
+
+    ros::NodeHandle nh_;
+    ros::Subscriber path_sub_;
+    ros::Subscriber octomap_sub_;
+    ros::Publisher occupancy_pub_;
+    octomap::OcTree* octree_;
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "path_occupancy_checker");
+
+    PathOccupancyChecker checker;
+
+    ros::spin();
+
+    return 0;
+}
+
+*/
+
+/*
+class PathOccupancyChecker
+{
+public:
+    PathOccupancyChecker()
+    {
+        // 订阅路径点话题
+        path_sub_ = nh_.subscribe("move_base_Quadruped/NavfnROS/plan", 1, &PathOccupancyChecker::pathCallback, this);
+
+        // 订阅OctoMap话题
+        octomap_sub_ = nh_.subscribe("octomap_full", 1, &PathOccupancyChecker::octomapCallback, this);
+
+        // 发布占用状态
+        occupancy_pub_ = nh_.advertise<std_msgs::Bool>("path_height_status", 1);
+
+        // 初始化标志
+        octree_ = nullptr;
+    }
+
+private:
+    void octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg)
+    {
+        // 将OctoMap消息转换为Octree对象
+        octomap::AbstractOcTree* abstract_tree = octomap_msgs::msgToMap(*msg);
+        octree_ = dynamic_cast<octomap::OcTree*>(abstract_tree);
+    }
+
+    void pathCallback(const nav_msgs::Path::ConstPtr& path)
+    {
+        if (!octree_)
+        {
+            ROS_WARN("OctoMap has not been received yet.");
+            return;
+        }
+
+        bool is_occupied = false;
+        double zmin = 0.0; // 最小高度
+        double zmax = 2.0; // 最大高度
+
+        // 使用ROS_INFO打印路径点的占用状态
+        for (const auto& pose : path->poses)
+        {
+            for (double z = zmin; z <= zmax; z += octree_->getResolution())
+            {
+                octomap::point3d coord(pose.pose.position.x, pose.pose.position.y, z);
+
+                // 查找对应的栅格
+                octomap::OcTreeNode* node = octree_->search(coord);
+
+                if (node)
+                {
+                    double occupancy_prob = node->getOccupancy();
+
+                    // 判断是否被占用
+                    if (occupancy_prob > 0.5) // 占用概率大于0.5表示被占用
+                    {
+                        ROS_INFO("Point (%.2f, %.2f, %.2f) is occupied.", pose.pose.position.x, pose.pose.position.y, z);
+                        is_occupied = true;
+                    }
+                    else
+                    {
+                        ROS_INFO("Point (%.2f, %.2f, %.2f) is not occupied.", pose.pose.position.x, pose.pose.position.y, z);
+                    }
+                }
+                else
+                {
+                    ROS_INFO("Point (%.2f, %.2f, %.2f) is not in the map.", pose.pose.position.x, pose.pose.position.y, z);
+                }
+            }
+        }
+
+        // 发布占用状态
+        std_msgs::Bool occupancy_msg;
+        occupancy_msg.data = is_occupied;
+        occupancy_pub_.publish(occupancy_msg);
+    }
+
+    ros::NodeHandle nh_;
+    ros::Subscriber path_sub_;
+    ros::Subscriber octomap_sub_;
+    ros::Publisher occupancy_pub_;
+    octomap::OcTree* octree_;
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "path_height_checker");
+
+    PathOccupancyChecker checker;
+
+    ros::spin();
+
+    return 0;
+}
+*/
+#include <std_msgs/Float32.h>
+class PathOccupancyChecker
+{
+public:
+    PathOccupancyChecker()
+    {
+        // 订阅路径点话题
+        path_sub_ = nh_.subscribe("move_base_Quadruped/NavfnROS/plan", 1, &PathOccupancyChecker::pathCallback, this);
+
+        // 订阅OctoMap话题
+        octomap_sub_ = nh_.subscribe("octomap_full", 1, &PathOccupancyChecker::octomapCallback, this);
+
+        // 发布高度状态
+        height_pub_ = nh_.advertise<std_msgs::Float32>("path_height_status", 1);
+
+        // 初始化标志
+        octree_ = nullptr;
+    }
+
+private:
+    void octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg)
+    {
+        // 将OctoMap消息转换为Octree对象
+        octomap::AbstractOcTree* abstract_tree = octomap_msgs::msgToMap(*msg);
+        octree_ = dynamic_cast<octomap::OcTree*>(abstract_tree);
+    }
+
+    void pathCallback(const nav_msgs::Path::ConstPtr& path)
+    {
+        if (!octree_)
+        {
+            ROS_WARN("OctoMap has not been received yet.");
+            return;
+        }
+
+        double height_threshold = 0.01; // 定义高度阈值
+        std_msgs::Float32 height_msg;
+        bool is_occupied = false;
+        double zmin = 0.0; // 最小高度
+        double zmax = 2.0; // 最大高度
+
+        // 使用ROS_INFO打印路径点的占用状态
+        for (const auto& pose : path->poses)
+        {
+            for (double z = 0.0; z <= octree_->getBBXMax().z(); z += octree_->getResolution())
+            {
+                octomap::point3d coord(pose.pose.position.x, pose.pose.position.y, z);
+
+                // 查找对应的栅格
+                octomap::OcTreeNode* node = octree_->search(coord);
+
+                if (node)
+                {
+                    double occupancy_prob = node->getOccupancy();
+
+                    // 如果占用概率大于0.5，并且高度超过阈值，则记录高度
+                    if (occupancy_prob > 0.5 && z > height_threshold)
+                    {
+                        ROS_INFO("Point (%.2f, %.2f, %.2f) exceeds height threshold. Height: %.2f", pose.pose.position.x, pose.pose.position.y, z, z);
+                        height_msg.data = z;
+                        height_pub_.publish(height_msg);
+                        return;
+                    }
+                }
+            }
+        // 使用ROS_INFO打印路径点的占用状态
+        for (const auto& pose : path->poses)
+        {
+            for (double z = zmin; z <= zmax; z += octree_->getResolution())
+            {
+                octomap::point3d coord(pose.pose.position.x, pose.pose.position.y, z);
+
+                // 查找对应的栅格
+                octomap::OcTreeNode* node = octree_->search(coord);
+
+                if (node)
+                {
+                    double occupancy_prob = node->getOccupancy();
+
+                    // 判断是否被占用
+                    if (occupancy_prob > 0.5) // 占用概率大于0.5表示被占用
+                    {
+                        ROS_INFO("Point (%.2f, %.2f, %.2f) is occupied.", pose.pose.position.x, pose.pose.position.y, z);
+                        is_occupied = true;
+                    }
+                    else
+                    {
+                        ROS_INFO("Point (%.2f, %.2f, %.2f) is not occupied.", pose.pose.position.x, pose.pose.position.y, z);
+                    }
+                }
+                else
+                {
+                    ROS_INFO("Point (%.2f, %.2f, %.2f) is not in the map.", pose.pose.position.x, pose.pose.position.y, z);
+                }
+            }
+        }
+
+
+
+        }
+
+        // 如果没有超过高度阈值的情况，则发布默认值
+        height_msg.data = 0.0;
+        height_pub_.publish(height_msg);
+    }
+
+    ros::NodeHandle nh_;
+    ros::Subscriber path_sub_;
+    ros::Subscriber octomap_sub_;
+    ros::Publisher height_pub_;
+    ros::Publisher occupancy_pub_;
+    octomap::OcTree* octree_;
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "path_height_checker");
+
+    PathOccupancyChecker checker;
+
+    ros::spin();
+
+    return 0;
+}
