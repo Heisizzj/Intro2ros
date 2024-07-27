81a37a48ace9ffff5f6843020515b25588e280e2
Run Yuan <75562909+MWAnnihilator@users.noreply.github.com>
2024-07-28 00:08:44 +0200

    state machine coupled with planning commands

--- AutonomousQuadruped/src/controller_pkg/src/state_machine_node.cpp
+++ AutonomousQuadruped/src/controller_pkg/src/state_machine_node.cpp
@@ -1,4 +1,3 @@
-
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
@@ -7,12 +6,13 @@
 #include <eigen3/Eigen/Dense>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Quaternion.h>
+#include <visualization_msgs/Marker.h>
 #include <tf/transform_datatypes.h>
 #include <tf/LinearMath/Matrix3x3.h>
 #include <state_indicator_msgs/state_indicator.h>
+#include <mav_msgs/Actuators.h>
 
 //state definition
 #define STANDBY 0
@@ -22,11 +22,10 @@
 #define CW_ROTATE 4
 #define RAISE_FORELEGS 5
 #define HIGHAMP_WALK 6
-#define END 7
+#define STEADYING 7
+#define END 8
 
-//define destination coordinates
-// change by Hao Guo for testing 2.5 9.5
-// 0.5 1.70
+//destination coordinates definition
 #define GOAL_X 2.5
 #define GOAL_Y 9.5
 
@@ -34,10 +33,11 @@
 
 class state_machine_node{
     ros::NodeHandle nh;
-    ros::Subscriber robot_pos;  //subscribe to current pose
-    ros::Subscriber path_point_sub;  //subscribe to path point   
-    ros::Publisher goal_pub, goal_vis_pub;  //publish goal to move base
-    ros::Publisher state_pub;   //publish state to controller
+    ros::Subscriber pose_est;  //subscribe to current pose
+    ros::Subscriber commands;  //subscribe to current move command 
+    
+    ros::NodeHandle n_;
+    ros::Publisher move_pub_;   //publish move key to controller
 
     ros::Timer timer;
     ros::Time start_time_1;
@@ -56,28 +56,27 @@ class state_machine_node{
 
     Eigen::Vector2d path_point;
     Eigen::Vector3d path_point_3d;
+    
+    Eigen::Vector2d command;
 
     geometry_msgs::PoseStamped goal;
-    visualization_msgs::Marker goal_marker;
-    int flag_forward = 1;
+
 public:
     state_machine_node():hz(200.0){
         // path_point, 
-
         state.state_msg = 0;  // STANDBY == 0
         // create a subscriber which can be used to subscribe the current pose
-        robot_pos = nh.subscribe("current_state_est", 1, &state_machine_node::GetPose, this);
+        pose_est = nh.subscribe("current_state_est", 1, &state_machine_node::GetPose, this);
         // create a subscriber which can be used to subscribe the current GetPathPoint
         path_point_sub = nh.subscribe("path_point",100,&state_machine_node::GetPathPoint,this);
+        // create a subscriber which can be used to subscribe the current GetPathPoint
+        commands = nh.subscribe("commands",1, &state_machine_node::GetCommand,this);
+        
         // the timer will be call 200 times per second
         timer = nh.createTimer(ros::Rate(hz), &state_machine_node::ControllLoop, this);
-        // create a publisher which can be used to publish goal
-        // /move_base_simple/goal
-        goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal",1);
 
         // create publishers which can be used to publish goal_marker and state
-        goal_vis_pub = nh.advertise<visualization_msgs::Marker>("goal_marker",1);
-        state_pub = nh.advertise<state_indicator_msgs::state_indicator>("state",1);
+        move_pub_ = n_.advertise<std_msgs::String>("key_input", 1);
 
         // define the goal pos
         goal.pose.position.x = GOAL_X;
@@ -86,20 +85,6 @@ public:
         goal.header.frame_id = "world";
         goal.pose.orientation.w = 1.0;
         destination << GOAL_X,GOAL_Y;
-
-        //visualize goal point in rviz
-        goal_marker.header.frame_id = "world";
-        goal_marker.ns = "state_machine_node";
-        goal_marker.id = 11;
-        goal_marker.action = visualization_msgs::Marker::ADD;
-        goal_marker.pose.orientation.w = 1.0;
-        goal_marker.type = visualization_msgs::Marker::CYLINDER;
-        goal_marker.scale.x = goal_marker.scale.y = goal_marker.scale.z = 0.2;
-        goal_marker.color.r = 1;
-        goal_marker.color.a = 1;
-        goal_marker.lifetime = ros::Duration();
-        geometry_msgs::Point p;
-        goal_marker.pose = goal.pose;
 
         ROS_INFO("state_machine_node start");
     }
@@ -134,6 +119,9 @@ public:
         path_point_3d(2) = path.z;
         start = 1;
     }
+    void GetCommand(const mav_msgs::Actuators& msg){
+    	command = msg.angular_velocities
+    }
 
     //check if the robot should rotate
     void check_rotation(Eigen::Vector2d &position, Eigen::Vector2d &orientation, Eigen::Vector2d &goal){
@@ -151,13 +139,6 @@ public:
         // the cos angle between the two vector
         cos = std::abs(distance_normalized.dot(orientation_normalized));
 
-        //  ROS_INFO("cos: %f", cos);
-        // std::string distStr = "[" + std::to_string(distance_normalized[0]) + ", " + std::to_string(distance_normalized[1]) + "]";
-        // ROS_INFO("Dist: %s", distStr.c_str());
-
-        // std::string oritStr = "[" + std::to_string(orientation_normalized[0]) + ", " + std::to_string(orientation_normalized[1]) + "]";
-        // ROS_INFO("Dist: %s", oritStr.c_str());
-
         // use cross product to difine the rotation direction
         if(orientation_normalized({0})*distance_normalized({1}) - orientation_normalized({1})*distance_normalized({0})<=0){
             state.state_msg = CW_ROTATE;
@@ -170,9 +151,7 @@ public:
             state.state_msg = FORWARD;
         }
     }
-    // only publish state
-    // if dont consider the jump, the state is only rotation and going forward. 
-    // no matter which state we currently in, we always check whether to change the state
+    
     void ControllLoop(const ros::TimerEvent& t){
     /*
     state_msg -> standby (0: default)
@@ -188,16 +167,12 @@ public:
       	case STANDBY: //initialize
             // publish the final goal position
             ROS_INFO("state: initial");
-            /*goal.header.stamp = goal_marker.header.stamp = ros::Time::now();
-            goal_pub.publish(goal);
-            goal_vis_pub.publish(goal_marker);
-            ROS_INFO_ONCE("goal published to move base"); */
             
             if(start == 1)
                 check_rotation(current_position, current_direction, path_point);
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
             
         case CCW_ROTATE:
@@ -205,7 +180,7 @@ public:
             check_rotation(current_position, current_direction, path_point);
             
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
 
         case CW_ROTATE:
@@ -213,14 +188,14 @@ public:
             check_rotation(current_position, current_direction, path_point);
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
         
         case END: //stop
             ROS_INFO("state: end");
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
         
         case FORWARD: //move forward. If there's a slope, try highamp. If it doesn't works, raise forelegs and trot upwards.
@@ -245,7 +220,7 @@ public:
                   highamp_count = 0;
                   climbing = 0;
                   check_rotation(current_position, current_direction, path_point);
-                  state_pub.publish(state);
+                  move_pub_.publish(state);
             }
 
             break;
@@ -257,7 +232,7 @@ public:
                 check_rotation(current_position, current_direction, path_point);
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
 	
 	case RAISE_FORELEGS: //raise forelegs, get on the slope
@@ -267,7 +242,7 @@ public:
                 check_rotation(current_position, current_direction, path_point);
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
         
         case HIGHAMP_WALK: //walk forward while legs been given high amplitude
@@ -278,7 +253,7 @@ public:
                 state.state_msg = JUMP;
 
             // publish state
-            state_pub.publish(state);
+            move_pub_.publish(state);
             break;
       }
     }
